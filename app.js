619d10a1b0665c635e04ad438e381a17b6a7cb5f
ESmithByui <smi20055@byui.edu>
2024-12-04 09:25:41 -0700

    updated icon and first project added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 const items = [
     { id: 1,
       title: "Item 1",
@@ -15,11 +16,15 @@ const items = [
       description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Massa libero accumsan aliquam taciti pharetra taciti porttitor elit. Dapibus varius praesent enim non facilisi vivamus. Mus posuere cursus malesuada pretium vitae interdum malesuada nullam. Odio pharetra scelerisque curae per cras elementum. Molestie quis aenean arcu sodales malesuada. Amet aptent sociosqu imperdiet eu malesuada arcu massa elit. Vestibulum cursus sollicitudin, conubia class etiam tempor. Malesuada class adipiscing luctus et lacus facilisis facilisis dolor.",
       url_destination: "GitHub Repository",
       url: null },
-    { id: 2, title: "Item 2", img: "/images/test2.png", description: "Details about Item 2", url_destination: "GitHub Repository", url: "https://esmithbyui.github.io/web-portfolio/personal-site/index.html" },
-    { id: 3, title: "Item 3", img: "/images/test3.png", description: "Details about Item 3", url_destination: "GitHub Repository", url: "https://esmithbyui.github.io/web-portfolio/personal-site/index.html" },
+    { id: 2,
+      title: "Cake Quest",
+      img: "/images/cake_quest.png",
+      description: "This is a game I created using the python pygame library. This game was simple controls, using the 'wasd' keys for controlling your slime character, with the 'q' key firing off a slimeball to hit enemies while the 'e' key allows the slime to slam down a hammer. As of writing this, there are 2 levels with enemies in them and 2 enemy types. In addition, there are variants of certain enemies, making them behave differently starting in level two. Overall, it adds a lot of fun into this game. I personally made all of the art for this game and utilized the silkscreen font. Overall, this project was a lot of fun and I keep working on it to add new features. As of now, there are death animations, better spawners that alert the player when they are about to spawn new enemies, and a spawning system that randomizes both types and variants of enemies. Overall, this is the project I'm most proud of.",
+      url_destination: "GitHub Repository",
+      url: "https://github.com/ESmithByui/Cake-Quest" },
+    
   ];
 const info = {items, item: null};
-console.log(info)
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
